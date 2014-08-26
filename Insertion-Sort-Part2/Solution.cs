64d0bbfa0a0b35fc9fb4889397ee937249ef8b67
mikkysati <mikkysati@gmail.com>
2014-08-26 14:50:34 +0530

    Create Solution.cs

---
+++ Insertion-Sort-Part2/Solution.cs
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+void insertionSort(int size, int *  ar) {
+    int i,j,t;
+    for(i=1;i<size;i++){
+        j=i;
+        while(ar[j]<ar[j-1]&&j>0){
+            t=ar[j];
+            ar[j]=ar[j-1];
+            ar[j-1]=t;
+            j--;
+        }
+        for(j=0;j<size;j++){
+            printf("%d ", ar[j]);
+        }
+        printf("\n");
+    }
+
+}
+int main(void) {
+   
+int size;
+scanf("%d", &size);
+int ar[size], i;
+for(i = 0; i < size; i++) { 
+   scanf("%d", &ar[i]); 
+}
+
+insertionSort(size, ar);
+   
+   return 0;
+}
